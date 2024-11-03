2786f42e22ee62c864a3ce3841c54bd984e3a0cc
Shomy <26857315+ShomyTKD@users.noreply.github.com>
2024-11-03 23:11:30 +0100

    Update port and fix cookies

--- services/auth/index.js
+++ services/auth/index.js
@@ -10,7 +10,7 @@ DB.init();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 app.use(express.json());
@@ -19,14 +19,6 @@ app.use(cookieParser());
 
 app.post('/api/v1/auth/signup', auth.signUp);
 app.post('/api/v1/auth/login', auth.login);
-
-app.get('/set-cookie', (req, res) => {
-    res.cookie('jwt', 'test-token', {
-        httpOnly: false,
-        secure: false,
-    });
-    res.send('cookie set')
-})
 
 app.listen(process.env.AUTH_PORT, (err) => {
     if (err) return console.log(err);
